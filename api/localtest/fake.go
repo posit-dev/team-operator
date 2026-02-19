5b0a13cf679e6f64e7e1acfdf7ad6626e3e723fe
ian-flores <iflores.siaca@posit.co>
2026-02-19 12:38:25 -0800

    fix: register status subresource in fake test client

--- api/localtest/fake.go
+++ api/localtest/fake.go
@@ -2,6 +2,7 @@ package localtest
 
 import (
 	"github.com/go-logr/logr"
+	v1beta1 "github.com/posit-dev/team-operator/api/core/v1beta1"
 	"github.com/posit-dev/team-operator/api/product"
 	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -11,10 +12,22 @@ import (
 type FakeTestEnv struct{}
 
 func (fte *FakeTestEnv) Start(loadSchemes func(scheme *runtime.Scheme)) (client.WithWatch, *runtime.Scheme, logr.Logger) {
-	cli := fakectrl.NewFakeClient()
-	cliScheme := cli.Scheme()
-	loadSchemes(cliScheme)
+	scheme := runtime.NewScheme()
+	loadSchemes(scheme)
+
+	cli := fakectrl.NewClientBuilder().
+		WithScheme(scheme).
+		WithStatusSubresource(
+			&v1beta1.Connect{},
+			&v1beta1.Workbench{},
+			&v1beta1.PackageManager{},
+			&v1beta1.Chronicle{},
+			&v1beta1.Flightdeck{},
+			&v1beta1.PostgresDatabase{},
+			&v1beta1.Site{},
+		).
+		Build()
 
 	log := product.NewSimpleLogger()
-	return cli, cliScheme, log
+	return cli, scheme, log
 }
